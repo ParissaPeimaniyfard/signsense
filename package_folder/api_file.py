14981e9ba3facd37b2805cbf4103d03aeaadb6c2
Christiane Morille <christiane.morille@gmx.de>
2024-03-20 20:47:21 +0100

    frontend fix and deploy in cloudrun

--- package_folder/api_file.py
+++ package_folder/api_file.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile
 from tensorflow.keras import models
 import numpy as np
-import requests
 from PIL import Image
 from google.cloud import storage
 from package_folder.train_model import ALPHABET
@@ -11,21 +10,19 @@ bucket = client.bucket('raw-data-signsense')
 blob = bucket.get_blob('model-params.h5')
 blob.download_to_filename('/tmp/model-params.h5')
 model_good = models.load_model('/tmp/model-params.h5')
-
+    
 app = FastAPI()
-
 
 ## Root Endpoint (Landing Page)
 @app.get("/")
-def root():
+def root(): 
     return {'greeting': "Hello User"}
 
 ## Predict Endpoint where model is located
 # ("/predict") specifies the URL specificity
-@app.route('/predict', methods=['POST'])
-def predict():
-    file = requests.files['image_file']
-    image = Image.open(file.stream)
+@app.post("/predict")
+def predict(image_file: UploadFile):
+    image = Image.open(image_file.file)
     image = image.resize((50, 50))
     img = np.array(image)
     img = img.reshape((-1, 50, 50, 3))
